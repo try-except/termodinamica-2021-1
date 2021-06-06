9ef63432b58b3938aac7fdbbdd786c4a896e0a87
Try Except <sjaramillo5@uc.cl>
2021-06-05 21:39:22 -0400

    analisis

--- Experimento 4/analisis.py
+++ Experimento 4/analisis.py
@@ -1,5 +1,8 @@
 # %%codecell
 import pandas as pd
+from IPython.display import display
+import matplotlib.pyplot as plt
+import numpy as np
 
 # %% codecell
 #leemos el excel y lo pasamos a un DataFrame de pandas
@@ -14,27 +17,37 @@ data.columns = data.columns.str.lower() #pasamos a minusculas
 data #vemos nuestro DataFrame
 
 # %% codecell
-#pasamos los parametros a variables
-d_p = 32.5 #mm
-m_pi_pla = 35 #g
-p_atm = 101325 #Pa
-m_cuerpo = 100.26 #g
+#pasamos los parametros a un dataframe separado
+unidades_parametros = data.iloc[0:4, 2].tolist()
+valores_parametros = data.iloc[0:4, 3].tolist()
+parametros = pd.DataFrame(
+    index=['valores'], columns=unidades_parametros
+)
+parametros.loc['valores'] = valores_parametros
+#limpiamos los nombres de las columnas
+parametros.columns = parametros.columns.str[:-6]
+parametros.columns = parametros.columns.str.strip('(')
+parametros.columns = parametros.columns.str.strip()
 
 #Y limpiamos el DataFrame
-data.drop(index=[0,1,2,3,4], inplace=True)
+data = data.drop(index=[0,1,2,3,4])
 data = data.dropna(how='any', axis=1) #borramos las columnas vacias
 
 #elegimos la primera fila como los nombres de las columnas
-data.columns = data.iloc[0]
-#Y la primera columna como los indices
-data = data.set_index(keys='Posición')
-
+nuevas_col = data.iloc[0].tolist()[1:]
 #borramos la fila de que contenía los nombres de las columnas
-data.drop(index=data.index[0],
-        axis=0,
-        inplace=True)
+data = data.drop(index=data.index[0], axis=0)
+nuevas_col = [''] + nuevas_col
+data.columns = nuevas_col
+
+#Y la primera columna como los indices
+data = data.set_index(keys='')
+data.index.name = 'pos' #la columna de los indices indica la posicion
 
 data.columns = data.columns.str[:-4] #eliminamos las unidades
 data.columns = data.columns.str.strip()
 
-data
+display(data)
+display(parametros)
+
+# %%codecell
